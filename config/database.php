112bceb11b2703b8c458477e5f5c931a736b832e
user1596025356 <sj85860102@gmail.com>
2020-04-13 14:57:08 +0800

    test

--- config/database.php
+++ config/database.php
@@ -23,7 +23,7 @@ return [
             // 服务器地址
             'hostname'          => env('database.hostname', '127.0.0.1'),
             // 数据库名
-            'database'          => env('database.database', 'creeper_ds918_top'),
+            'database'          => env('database.database', 'creeper_ds918_to'),
             // 用户名
             'username'          => env('database.username', 'root'),
             // 密码
